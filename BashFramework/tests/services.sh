abe7d2cfb9f87383b684ab31481cee9353058b96
cantr1 <kellycantrelly@gmail.com>
2025-09-16 17:37:43 -0500

    Added services feature

---
+++ BashFramework/tests/services.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -a SERVICES=("sshd" "firewalld")
+
+test_services() {
+    for SERVICE in "${SERVICES[@]}"; do
+        result=$(sudo systemctl is-active "$SERVICE")
+        case "$result" in
+            "active")
+                assert_true "[[ \"$result\" == \"active\" ]]" "Service $SERVICE active"
+                ;;
+            "inactive")
+                assert_true "[[ \"$result\" == \"active\" ]]" "Service $SERVICE inactive"
+                ;;
+            *)
+                echo "Service in unknown state..."
+        esac
+    done
+}
+
+test_services
