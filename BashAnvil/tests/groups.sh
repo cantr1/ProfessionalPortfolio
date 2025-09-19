51e9530f7cd7eae0a170834bd5615993ec9a4224
cantr1 <kellycantrelly@gmail.com>
2025-09-19 17:48:43 -0500

    Modified group check

--- BashAnvil/tests/groups.sh
+++ BashAnvil/tests/groups.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Checks expected groups were created during the playbook
 
-declare -a GROUPS=("devops" "security" "developers")
-
 test_groups() {
-    for GROUP in "${GROUPS[@]}"; do
+    for GROUP in "devops" "security" "developers"; do
         result=$(getent group "$GROUP")
         if [[ -n "$result" ]]; then 
             assert_true "[[ -n \"$result\" ]]" "Group $GROUP setup"
         else
-            assert_true "[[ -n \"$result\" ]]" "Group $GROUP not installed"
+            assert_true "false" "Group $GROUP not installed"
         fi
     done
 }
