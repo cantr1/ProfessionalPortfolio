0031b3bd653f08bc8fb8745160706faef361ed1c
cantr1 <kellycantrelly@gmail.com>
2025-09-20 07:02:17 -0500

    Playbooks setup

---
+++ BashAnvil/setup_scripts/setup_dhcp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+IP=$(hostname -I | awk '{print $1}')
+
+THIRD_OCTET=$(echo "$IP" | cut -d '.' -f1-3)
+
+
+sudo tee /etc/dhcp/dhcpd.conf > /dev/null << EOF
+subnet $THIRD_OCTET.0 netmask 255.255.255.0 {
+  range 192.168.1.100 192.168.1.200;
+  option routers $IP;
+  option domain-name-servers 8.8.8.8, 8.8.4.4;
+  default-lease-time 600;
+  max-lease-time 7200;
+}
+EOF
+
+sudo systemctl restart isc-dhcp-server
+
+echo "🛸 DHCP Now Setup"
---
+++ BashAnvil/setup_scripts/setup_mqtt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script sets up the MQTT to allow the server to be a broker
+set -e
+
+sudo tee /etc/mosquitto/mosquitto.conf > /dev/null << EOF
+listener 1883
+allow_anonymous true
+EOF
+
+sudo systemctl restart mosquitto
+
+echo "📡 MQTT Now Setup"
