0f1708eb23112c9927c93ae39272f74922cb27cd
Kelly Cantrell <kellycantrelly@gmail.com>
2026-01-31 10:59:20 -0600

    Added logging

--- WIP/main.py
+++ WIP/main.py
@@ -3,13 +3,25 @@ from redis.asyncio import Redis
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
 
 redis_server = os.getenv("REDIS_HOST")
 redis_port = os.getenv("REDIS_PORT")
+log_path = os.getenv("LOG_PATH")
 
 app = FastAPI()
+
+# Suppress repetitive watchfiles logs unless actual errors
+logging.getLogger("watchfiles").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename=log_path,
+    filemode='a'
+)
 
 
 class KV(BaseModel):
@@ -24,22 +36,27 @@ async def startup():
                   port=redis_port,
                   decode_responses=True)
 
+    logging.info("==== API Started ====")
+
 
 @app.get("/")
 async def root():
     """Just a way to quickly check API connectivity"""
+    logging.info("Connectivity Confirmed - API Accepting Requests")
     return {"message": "Hello, Python!"}
 
 
 @app.post("/set")
 async def set_key(kv: KV):
     await redis.set(kv.key, kv.value)
+    logging.info(f"Set Method Called - ({kv.key}: {kv.value})")
     return {"ok": True}
 
 
 @app.get("/get/{key}")
 async def get_key(kv: KV):
     value = await redis.get(kv.key)
+    logging.info(f"Get Method Called on ({kv.key}) - Returning {value})")
     return {"value": value}
 
 
