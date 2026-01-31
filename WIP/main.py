b37a9b67d72ec47573075bdb868fadc8a3cb0840
Kelly Cantrell <kellycantrelly@gmail.com>
2026-01-31 16:53:57 -0600

    Created task queue

--- WIP/main.py
+++ WIP/main.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 import logging
+import uuid
+import json
 
 load_dotenv()
 
@@ -39,25 +41,39 @@ async def startup():
     logging.info("======== API Started ========")
 
 
-@app.post("/set")
-async def set_key(kv: KV):
-    await redis.set(kv.key, kv.value)
-    logging.info(f"SET - ({kv.key}: {kv.value})")
-    return {"ok": True}
+@app.post("/create_task")
+async def create_task(kv: KV):
+    if kv.key == "task":
+        logging.info(f"Recieved Task - ({kv.value})")
+        task_id = str(uuid.uuid4())
+        logging.info(f"Generating Task ID# ({task_id})")
+        task = {
+            "id": task_id,
+            "value": kv.value
+        }
+        await redis.rpush("task_queue", json.dumps(task))
+        logger.info(f"Task Created: {task["id"]}: {task["value"]}")
+        return {"task_id": task["id"]}
 
 
-@app.get("/get/{key}")
-async def get_key(kv: KV):
-    value = await redis.get(kv.key)
-    logging.info(f"GET: ({kv.key}) - Returning: {value})")
-    return {"value": value}
+@app.get("/get_task")
+async def get_task():
+    logging.info("Task Requested")
+    task_json = await redis.lpop("task_queue")
+
+    if task_json is None:
+        logger.info("No active tasks found")
+        return {"message": "no tasks"}
+
+    task = json.loads(task_json)
+    logger.info(f"Task Accepted - {task['id']}")
+
+    return task
 
 
-@app.delete("/delete/{key}")
-async def remove_key(kv: KV):
-    value = await redis.delete(kv.key)
-    logging.warning(f"DELETE - ({kv.key})")
-    return {"deleted": value}
+@app.post("/complete_task")
+async def complete_task(kv: KV):
+    pass
 
 
 @app.on_event("shutdown")
